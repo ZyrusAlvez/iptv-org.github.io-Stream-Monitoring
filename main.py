9fb6e9379ac1bd2dc794eedb50fb7f4d3d5dc5c2
ZyrusAlvez <zyrusalvez13@gmail.com>
2025-07-07 12:07:16 +0800

    up or down

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 with sync_playwright() as p:
     browser = p.chromium.launch(headless=True)
     page = browser.new_page()
-    
+
     # Go to the page
     page.goto("https://iptv-org.github.io/channels/al/3Plus#SD", timeout=60000)
 
@@ -14,11 +14,16 @@ with sync_playwright() as p:
     page.wait_for_selector('a[target="_blank"][rel="noopener noreferrer"]', timeout=10000)
 
     # Get all matching <a> tags
-    links = page.query_selector_all('a[target="_blank"][rel="noopener noreferrer"][class="whitespace-nowrap text-sm hover:text-blue-500 dark:hover:text-blue-400 hover:underline"]')
-    
+    links = page.query_selector_all(
+        'a[target="_blank"][rel="noopener noreferrer"][class="whitespace-nowrap text-sm hover:text-blue-500 dark:hover:text-blue-400 hover:underline"]'
+    )
+
     for link in links:
-        text = link.text_content()
+        text = link.text_content().strip()
         href = link.get_attribute("href")
-        print(f"Text: {text.strip()}")
+        if href and href.endswith(".m3u8"):
+            print(f"Text: {text} - UP")
+        else:
+            print(f"Text: {text} - DOWN")
 
     browser.close()
